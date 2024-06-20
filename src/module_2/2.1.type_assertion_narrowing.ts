3e7e872ce509a8496b9168b29d3618cf3dc2f980
sumoncse19 <mdsumoncse19@gmail.com>
2024-06-21 03:26:40 +0600

    type alias interface assertion and narrowing

--- src/module_2/2.1.type_assertion_narrowing.ts
+++ src/module_2/2.1.type_assertion_narrowing.ts
@@ -1,1 +1,36 @@
-console.log("Type assertion check");
+{
+  /*
+  <--------- type assertion --------->
+  */
+
+  let anything: any;
+  anything = 10;
+  anything = "Sumon";
+  anything = true;
+
+  // (anything as number).
+  // (anything as string).
+  // (anything as boolean).
+
+  const kgToGm = (value: string | number): string | number | undefined => {
+    if (typeof value === "string") {
+      const convertedValue = parseFloat(value) * 1000;
+      return `The converted value is ${convertedValue}`;
+    } else if (typeof value === "number") {
+      return value * 1000;
+    }
+  };
+
+  const result1 = kgToGm(1000) as number;
+  const result2 = kgToGm("1000") as string;
+
+  type CustomError = {
+    message: string;
+  };
+
+  try {
+    console.log("From try");
+  } catch (error) {
+    console.log((error as CustomError).message);
+  }
+}
---
+++ src/module_2/2.2.interface.ts
@@ -0,0 +1,53 @@
+{
+  /*
+  <--------- type interface --------->
+  */
+
+  type User1 = {
+    name: string;
+    age: number;
+  };
+
+  interface User2 {
+    name: string;
+    age: number;
+  }
+
+  // Extend with type alias
+  type UserWithRole1 = User1 & { role: string };
+
+  // Extend with type interface
+  // interface UserWithRole2 extends User2 {
+  interface UserWithRole2 extends User1 {
+    role: string;
+  }
+
+  const user1: UserWithRole2 = {
+    name: "Persian",
+    age: 100,
+    role: "manager",
+  };
+
+  type rollNumber = number;
+
+  // js --> object , array-> object function -> object
+
+  type Roll1 = number[];
+
+  interface Roll2 {
+    [index: number]: number;
+  }
+
+  const rollNumber1: Roll2 = [1, 2, 3];
+  // ---------------------->  0  1  2 ---> number type
+
+  type Add1 = (num1: number, num2: number) => number;
+
+  interface Add2 {
+    (num1: number, num2: number): number;
+  }
+
+  const add: Add2 = (num1, num2) => num1 + num2;
+
+  //
+}
